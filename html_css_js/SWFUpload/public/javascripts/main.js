d42687e444a81d8d1de2219e110da9b8086839ed
agate <agate.hao@gmail.com>
2009-07-16 17:30:18 +0800

    added progress bar

--- html_css_js/SWFUpload/public/javascripts/main.js
+++ html_css_js/SWFUpload/public/javascripts/main.js
@@ -2,15 +2,24 @@
  * callback steps
  *
  * 1. swfuploadLoaded
+ *
  * 2. fileDialogStart -- when you clicked the button
- * 3. fileQueued [n+] -- when you selected files and clicked the ok
+ *
+ * ----------- [3] one more times ---------------
+ * 3. fileQueued -- when you selected files and clicked the ok
+ * ----------- [3] one more times ---------------
+ *
  * 4. fileDialogComplete
- * ----------- [5 - 7] one more times ---------------
+ *
+ * ----------- [5 - 6] one more times -----------
  * 5. uploadStart
+ *
  * 6. uploadProgress
  *    a. start    -- byte 0 -- total byte X
  *    b. progress -- byte n -- total byte X
  *    c. finished -- byte X -- total byte X
+ * ----------- [5 - 6] one more times -----------
+ *
  * 7. uploadComplete
  *
  */
@@ -47,16 +56,19 @@ var Uploader = function () {}; Uploader.prototype = {
 
 
       // callbacks
-      swfupload_loaded_handler:     function () { self.swfuploadLoaded(arguments); },
-      file_dialog_start_handler:    function () { self.fileDialogStart(arguments); },
-      file_dialog_complete_handler: function () { self.fileDialogComplete(arguments); },
-      file_queued_handler:          function () { self.fileQueued(arguments); },
-      file_queue_error_handler:     function () { self.fileQueueError(arguments); },
-      upload_start_handler:         function () { self.uploadStart(arguments); },
-      upload_progress_handler:      function () { self.uploadProgress(arguments); },
-      upload_error_handler:         function () { self.uploadError(arguments); },
-      upload_success_handler:       function () { self.uploadSuccess(arguments); },
-      upload_complete_handler:      function () { self.uploadComplete(arguments); }
+      swfupload_loaded_handler:     function () { self.swfuploadLoaded(); },
+
+      file_dialog_start_handler:    function () { self.fileDialogStart(); },
+      file_queued_handler:          function (file) { self.fileQueued(arguments); },
+      file_queue_error_handler:     function (file, errorCode, message) { self.fileQueueError(file, errorCode, message); },
+      file_dialog_complete_handler: function (selectedNum, queuedNum, totalQueuedNum) { self.fileDialogComplete(selectedNum, queuedNum, totalQueuedNum); },
+
+      upload_start_handler:         function (file) { self.uploadStart(file); },
+      upload_progress_handler:      function (file, complete, total) { self.uploadProgress(file, complete, total); },
+      upload_error_handler:         function (file, errorCode, message) { self.uploadError(file, errorCode, message); },
+      upload_success_handler:       function (file, serverData, response) { self.uploadSuccess(file, serverData, response); },
+
+      upload_complete_handler:      function (file) { self.uploadComplete(file); }
     });
   },
 
---
+++ html_css_js/progress_bar/progress_bar.js
@@ -0,0 +1,53 @@
+/**
+ *  options:
+ *    color: progressBar's background color
+ *    width: progressBar's width
+ *
+ *  manual:
+ *    var p = new ProgressBar(selector);
+ *    p.setPercent(23);
+ */
+
+var ProgressBar = function () { this.initialize.apply(this, arguments); }; ProgressBar.prototype = {
+  DEFAULT_COLOR: "blue",
+  DEFAULT_WIDTH: 200,
+
+  initialize: function (jq, options) {
+    this.jq      = jq;
+    this.options = options || {};
+
+    this.initHTML();
+  },
+
+  initHTML: function () {
+    this.jq.html('<div class="progressBar"><div class="bar" /><div class="textContainer">0%</div></div>');
+
+    this.jqBorder = this.jq.find('.progressBar:first');
+    this.jqBar    = this.jq.find('.bar:first');
+    this.jqText   = this.jq.find('.textContainer');
+
+    var color = this.options.color || this.DEFAULT_COLOR;
+    var width = this.options.width || this.DEFAULT_WIDTH;
+
+    this.setColor(color);
+    this.setWidth(width);
+  },
+  
+  setColor: function (color) {
+    this.color = color;
+    this.jqBorder.css('border-color', color);
+    this.jqBar.css('background-color', color);
+  },
+
+  setWidth: function (width) {
+    this.width = width;
+    this.jqBorder.width(width);
+    this.jqText.css('left', width + 10);
+  },
+
+  setPercent: function (percent) {
+    var width = percent * (this.width - 2) / 100;
+    this.jqBar.width(width);
+    this.jqText.html(percent + '%');
+  }
+}
